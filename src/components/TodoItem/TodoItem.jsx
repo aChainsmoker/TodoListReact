d3f713b149d62eb5cd57dcbcada343f9b8d502ce
Ivan <neborsky.ivan@tut.by>
2026-06-25 17:22:09 +0300

    fix: make a separate callback function for deleting and toggling

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,26 +1,35 @@
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import { useCallback } from 'react';
 
 import './TodoItem.css';
 
-export default function TodoItem ({ todo, onDelete, onToggle }) {
+export default function TodoItem({ todo, onDelete, onToggle }) {
+  const handleDelete = useCallback(() => {
+    onDelete(todo.id);
+  }, [onDelete, todo.id]);
+
+  const handleToggle = useCallback(() => {
+    onDelete(todo.id);
+  }, [onToggle, todo.id]);
+
   return (
     <li className="todo-item">
       <span className={classNames('todo-item__text', { 'todo-item__text--completed': todo.completed })}>
         {todo.text}
       </span>
       <div className="todo-item__actions">
-        <button 
-          type="button" 
-          className="todo-item__button todo-item__button--delete" 
-          onClick={() => onDelete(todo.id)}
+        <button
+          type="button"
+          className="todo-item__button todo-item__button--delete"
+          onClick={handleDelete}
         >
           Delete
         </button>
-        <button 
-          type="button" 
-          className="todo-item__button todo-item__button--toggle" 
-          onClick={() => onToggle(todo.id)}
+        <button
+          type="button"
+          className="todo-item__button todo-item__button--toggle"
+          onClick={handleToggle}
         >
           {!todo.completed ? "Done" : "Undo"}
         </button>
